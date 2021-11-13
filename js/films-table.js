594e4876713e89217cd85fcbfdf3bfb60ff0b4cf
KamilGimazitdinov <k.gimazitdinov@gmail.com>
2021-11-13 15:52:47 +0400

    тз-16

--- js/films-table.js
+++ js/films-table.js
@@ -1,24 +1,29 @@
 const films = [
     { 
         time: "10:00", 
+        adult: true,
         name: "Человек-паук", 
         genres: [ "фантастика", "боевик", "приключения"]    
     },
 
     { 
         time: "12:00", 
+        rate: "R",
+        adult: true,
         name: "Собачья жизнь 2", 
         genres: [ "фэнтэзи", "драма", "комедия"]    
     },
 
     { 
         time: "14:00", 
+        rate: "G",
         name: "История игрушек 4", 
         genres: [ "мультфильм", "фэнтэзи", "комедия"]    
     },
 
     { 
         time: "16:00", 
+        rate: "NC-17",
         name: "Люди в чёрном: Интэрнэшнл", 
         genres: [ "фантастика", "боевик", "комедия"]    
     }
@@ -29,13 +34,24 @@ console.log (div.innerHTML);
 
 div.innerHTML = '';
 
-for (let index = 0; index < films.length; index++) {
-    div.innerHTML += `
-    <div class="table_check table_row table_row_dark"><img src="./images/tablica/galochkawh.svg" class="table_icon-size" alt="галочка"></div>
-    <div class="table_cell table_row table_row_dark">${films[index].time}</div>
-    <div class="table_cell table_row table_row_dark"><a href="https://www.kinopoisk.ru/film/838/" target="_block" class="table_references">${films[index].name}</a></div>
-    <div class="table_cell table_row table_row_dark">${films[index].genres.join(', ')}</div>
-`;
+function renderFilmTableItem(film){
+
+return `
+    <div class="table_check table_row"><img src="./images/tablica/galochkawh.svg" class="table_icon-size" alt="галочка"></div>
+    <div class="table_cell table_row">${film.time}</div>
+    <div class="table_cell table_row"><a href="https://www.kinopoisk.ru/film/838/" target="_block" class="table_references">${film.name}</a></div>
+    <div class="table_cell table_row">${film.genres.join(', ')}</div>
+    `
+} 
+
+for (let film of films) {
+    console.log(film);
+
+    if (!(film.rate === 'R' || film.rate === 'NC-17')){
+        div.innerHTML += renderFilmTableItem(film);
+    }
+
+    
 }
 
 // div.innerHTML = `
